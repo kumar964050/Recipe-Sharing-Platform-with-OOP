d530194dbef1633552a6a88189dace6e40870d58
kumar964050 <kumar964050@gmail.com>
2024-03-07 19:56:24 +0530

    convert into a class

--- controllers/user.js
+++ controllers/user.js
@@ -1,114 +1,118 @@
 const User = require("../models/User");
-// register
-exports.register = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
 
-    if (!(username && password)) {
-      return res.status(400).json({
-        msg: "Please enter a username & password",
+class UserController {
+  // register a new user
+  async register(req, res, next) {
+    try {
+      const { username, password } = req.body;
+
+      if (!(username && password)) {
+        return res.status(400).json({
+          msg: "Please enter a username & password",
+        });
+      }
+      const user = new User(username, password);
+      const result = await user.save();
+
+      result.password = undefined;
+      res.status(201).json({
+        msg: "User created successfully",
+        user: result,
       });
+    } catch (error) {
+      next(
+        res.status(500).json({
+          msg: error.message,
+        })
+      );
     }
-    const user = new User(username, password);
-    const result = await user.save();
-
-    result.password = undefined;
-    res.status(201).json({
-      msg: "User created successfully",
-      user: result,
-    });
-  } catch (error) {
-    next(
-      res.status(500).json({
-        msg: error.message,
-      })
-    );
   }
-};
-// login
-exports.login = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
+  // login a user
+  async login(req, res, next) {
+    try {
+      const { username, password } = req.body;
 
-    if (!(username && password)) {
-      return res.status(400).json({
-        msg: "Please enter a username & password",
+      if (!(username && password)) {
+        return res.status(400).json({
+          msg: "Please enter a username & password",
+        });
+      }
+      const user = new User(username, password);
+      const result = await user.authenticate();
+      result.password = undefined;
+      res.status(201).json({
+        msg: "User created successfully",
+        user: result,
+        token: user.generateJwtToken(),
       });
+    } catch (error) {
+      next(
+        res.status(500).json({
+          msg: error.message,
+        })
+      );
     }
-    const user = new User(username, password);
-    const result = await user.authenticate();
-    result.password = undefined;
-    res.status(201).json({
-      msg: "User created successfully",
-      user: result,
-      token: user.generateJwtToken(),
-    });
-  } catch (error) {
-    next(
-      res.status(500).json({
-        msg: error.message,
-      })
-    );
   }
-};
-// my profile
-exports.profile = async (req, res, next) => {
-  try {
-    const user = req.user;
-    user.password = undefined;
-    res.status(200).json({
-      user: user,
-      msg: "success",
-    });
-  } catch (error) {
-    next(
-      res.status(500).json({
-        msg: error.message,
-      })
-    );
-  }
-};
-// update
-exports.updateProfile = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-
-    // setting previous values
-    const user = new User(req.user.username, req.user.password);
-
-    if (!username && !password) {
-      return res
-        .status(400)
-        .json({ msg: "Please provide username or password" });
+  // my profile
+  async profile(req, res, next) {
+    try {
+      const user = req.user;
+      user.password = undefined;
+      res.status(200).json({
+        user: user,
+        msg: "success",
+      });
+    } catch (error) {
+      next(
+        res.status(500).json({
+          msg: error.message,
+        })
+      );
     }
-
-    if (username) await user.updateUsername(username);
-    if (password) await user.updatePassword(password);
-    res.status(201).json({
-      msg: "update successfully",
-    });
-  } catch (error) {
-    next(
-      res.status(500).json({
-        msg: error.message,
-      })
-    );
   }
-};
+  // update my profile
+  async updateProfile(req, res, next) {
+    try {
+      const { username, password } = req.body;
 
-// delete my profile
-exports.deleteProfile = async (req, res, next) => {
-  try {
-    const user = new User(req.user.username);
-    user.deleteUser();
-    res.status(200).json({
-      msg: "delete successfully",
-    });
-  } catch (error) {
-    next(
-      res.status(500).json({
-        msg: error.message,
-      })
-    );
+      // setting previous values
+      const user = new User(req.user.username, req.user.password);
+
+      if (!username && !password) {
+        return res
+          .status(400)
+          .json({ msg: "Please provide username or password" });
+      }
+
+      if (username) await user.updateUsername(username);
+      if (password) await user.updatePassword(password);
+      res.status(201).json({
+        msg: "update successfully",
+      });
+    } catch (error) {
+      next(
+        res.status(500).json({
+          msg: error.message,
+        })
+      );
+    }
   }
-};
+  // delete my profile
+  async deleteProfile(req, res, next) {
+    try {
+      const user = new User(req.user.username);
+      user.deleteUser();
+      res.status(200).json({
+        msg: "delete successfully",
+      });
+    } catch (error) {
+      next(
+        res.status(500).json({
+          msg: error.message,
+        })
+      );
+    }
+  }
+}
+
+module.exports = new UserController();
